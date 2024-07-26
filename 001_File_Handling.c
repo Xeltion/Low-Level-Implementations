7fba31a9e3f78a4b4bc355d219c5a2f9b5c31929
Niskarsh <www.xeltacros1412@gmail.com>
2024-07-27 03:54:40 +0530

    Intial commit for the File Handling Code

---
+++ 001_File_Handling.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_FILENAME_LENGTH 256
+#define DEAULT_DATA 25
+
+int main(int argc, char *argv[]){
+
+    FILE *filePointer;
+    char fileName[MAX_FILENAME_LENGTH];
+    int data = DEAULT_DATA;
+
+    fgets(fileName, MAX_FILENAME_LENGTH, stdin);
+    //filename[strcspn(filename, "\n")] = 0;
+    fileName[strlen(fileName) - 1] = '\0';
+
+    filePointer = fopen(fileName,"w+");
+
+    if(filePointer == NULL){
+        perror("Error");
+        return 1;
+    }
+
+    fprintf(filePointer, "%d", data);
+    rewind(filePointer);
+    fscanf(filePointer, "%d", &data);
+
+    fclose(filePointer);
+
+    printf("This is the %s. And its content:- %d", fileName, data);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
